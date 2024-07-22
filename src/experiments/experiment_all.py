32f9930f59537d9048892c36aa7f075cd0d9be98
hsmmi <hesoyam.nomad@gmail.com>
2024-07-22 10:22:18 +0330

    update experiment all

--- src/experiments/experiment_all.py
+++ src/experiments/experiment_all.py
@@ -1,3 +1,4 @@
+import numpy as np
 from src.utils.result import log_result
 from src.utils.data_preprocessing import load_data
 from src.utils.evaluation_metrics import compare_prototype_selection
@@ -11,14 +12,14 @@ from src.algorithms.lssm import LSSm
 from src.algorithms.ris import RIS
 from src.algorithms.hmnei import HMNEI
 from src.algorithms.nngir import NNGIR
+import tqdm
 
-DATASET_NAME = "wine"
+DATASET_NAME = "digits"
 
 # Get file name
 FILE_NAME = __file__.split("/")[-1].split(".")[0]
 
-# Load dataset
-X, y = load_data(DATASET_NAME)
+datasets = ["iris", "wine", "moons_0.15", "circles_0.05"]
 
 algorithms = {
     "CNN": {"algorithm": CNN().fit_transform},
@@ -34,6 +35,19 @@ algorithms = {
     "NNGIR": {"algorithm": NNGIR().fit_transform},
 }
 
-result = compare_prototype_selection(X, y, algorithms, 1, 10)
+tmp_results = []
 
-log_result(result, FILE_NAME, DATASET_NAME)
+for dataset_name in tqdm.tqdm(datasets, desc="Dataset progress", leave=False):
+    X, y = load_data(dataset_name)
+
+    tmp_results.append(compare_prototype_selection(X, y, algorithms, 1, 10))
+
+result = {}
+for algorithm_name, _ in tmp_results[0].items():
+    result[algorithm_name] = []
+    for tmp_result in tmp_results:
+        result[algorithm_name].append(tmp_result[algorithm_name])
+
+    result[algorithm_name] = np.array(result[algorithm_name]).mean(axis=0)
+
+log_result(result, FILE_NAME, datasets)
